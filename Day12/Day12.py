8d9535724dcf54aaeacbb8a0dc24a38e4e05aaaa
Chi-Kit Pao <Chi-KitPao@gmx.de>
2023-12-13 01:34:52 +0100

    Added Solution for AoC 2023, Day 12, Part 2
    Improved algorithm also for part 1

--- Day12/Day12.py
+++ Day12/Day12.py
@@ -3,7 +3,7 @@
     Author: Chi-Kit Pao
 """
 
-import itertools
+import functools
 import os
 import time
 
@@ -14,20 +14,61 @@ def parse_line(line):
     return pattern, number_values
 
 
-def test_pattern(pattern, number_values):
-    pattern_list = list(pattern)
-    question_mark_positions = [
-        i for i in range(len(pattern_list)) if pattern_list[i] == "?"
-    ]
+@functools.cache
+def test_pattern(pattern, damages):
     result = 0
-    for p in itertools.product((".", "#"), repeat=len(question_mark_positions)):
-        for i, pos in enumerate(question_mark_positions):
-            pattern_list[pos] = p[i]
-        damage_count = tuple(
-            [len(list(g)) for k, g in itertools.groupby(pattern_list) if k == "#"]
+    current_damage = damages[0]
+    remaining_damages = damages[1:]
+    if len(remaining_damages) > 0:
+        min_remaining_pattern_length = (
+            sum(remaining_damages) + len(remaining_damages) - 1
         )
-        if damage_count == number_values:
-            result += 1
+    else:
+        min_remaining_pattern_length = 0
+    for i in range(len(pattern) - current_damage - min_remaining_pattern_length + 1):
+        if pattern[i] == "#":
+            # Damage must start here.
+            if all(map(lambda c: c in ("?", "#"), pattern[i : i + current_damage])):
+                if len(remaining_damages) > 0:
+                    if pattern[i + current_damage] in ("?", "."):
+                        result += test_pattern(
+                            pattern[i + current_damage + 1 :], remaining_damages
+                        )
+                else:
+                    assert i + current_damage <= len(pattern)
+                    if i + current_damage == len(pattern):
+                        result += 1
+                    else:
+                        if all(
+                            map(
+                                lambda c: c in ("?", "."), pattern[i + current_damage :]
+                            )
+                        ):
+                            result += 1
+            break
+        elif pattern[i] == "?":
+            # Damage might start here.
+            if all(map(lambda c: c in ("?", "#"), pattern[i : i + current_damage])):
+                if len(remaining_damages) > 0:
+                    if pattern[i + current_damage] in ("?", "."):
+                        result += test_pattern(
+                            pattern[i + current_damage + 1 :], remaining_damages
+                        )
+                else:
+                    assert i + current_damage <= len(pattern)
+                    if i + current_damage == len(pattern):
+                        result += 1
+                    else:
+                        if all(
+                            map(
+                                lambda c: c in ("?", "."), pattern[i + current_damage :]
+                            )
+                        ):
+                            result += 1
+            continue
+        else:
+            # Found ".". Iterate further since damage doesn't start here.
+            continue
     return result
 
 
@@ -36,10 +77,33 @@ def main():
 
     with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as f:
         lines = list(map(lambda s: s.replace("\n", ""), f.readlines()))
-    result1 = 0
+
+    # Part 1
+    pattern_list = []
+    damages_list = []
     for line in lines:
-        pattern, number_values = parse_line(line)
-        result1 += test_pattern(pattern, number_values)
+        pattern, damages = parse_line(line)
+        pattern_list.append(pattern)
+        damages_list.append(damages)
+
+    result1 = 0
+    for i in range(len(pattern_list)):
+        temp = test_pattern(pattern_list[i], damages_list[i])
+        result1 += temp
+
+    # Part 2
+    pattern_list2 = []
+    damages_list2 = []
+    for i in range(len(pattern_list)):
+        pattern2 = (pattern_list[i] + "?") * 4 + pattern_list[i]
+        pattern_list2.append(pattern2)
+        damages2 = damages_list[i] * 5
+        damages_list2.append(damages2)
+
+    result2 = 0
+    for i in range(len(pattern_list2)):
+        temp = test_pattern(pattern_list2[i], damages_list2[i])
+        result2 += temp
 
     print(
         "Question 1: For each row, count all of the different arrangements of\n"
@@ -47,11 +111,11 @@ def main():
         " is the sum of those counts?"
     )
     print(f"Answer: {result1}")
-    #print(
-    #    "Question 2: Unfold your condition records; what is the new sum of\n"
-    #    " possible arrangement counts?"
-    #)
-    #print(f"Answer: {result2}")
+    print(
+        "Question 2: Unfold your condition records; what is the new sum of\n"
+        " possible arrangement counts?"
+    )
+    print(f"Answer: {result2}")
     print(f"Time elapsed: {time.time() - start_time} s")
 
 
@@ -62,4 +126,7 @@ if __name__ == "__main__":
 #  operational and broken springs that meet the given criteria. What
 #  is the sum of those counts?
 # Answer: 7653
-# Time elapsed: 16.910554885864258 s
+# Question 2: Unfold your condition records; what is the new sum of
+#  possible arrangement counts?
+# Answer: 60681419004564
+# Time elapsed: 0.455655574798584 s
